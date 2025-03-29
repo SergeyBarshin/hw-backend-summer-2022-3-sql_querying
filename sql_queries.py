e50414a559ff3d188b8f12458247a8144eba9ac9
SergeyBarshin <sergey.barshin@yandex.ru>
2025-03-30 00:44:45 +0300

    attemt 1

--- sql_queries.py
+++ sql_queries.py
@@ -2,6 +2,11 @@
 # Duration - разница между scheduled_arrival и scheduled_departure.
 # В ответе должно быть 2 колонки [flight_no, duration]
 TASK_1_QUERY = """
+select flight_no, (scheduled_arrival-scheduled_departure) as duration
+from flights
+where (scheduled_arrival-scheduled_departure) is not null
+order by duration
+limit 5;
 """
 #  flight_no | duration
 # -----------+----------
@@ -16,17 +21,15 @@ TASK_1_QUERY = """
 # количество упоминаний которых меньше 50
 # В ответе должно быть 2 колонки [flight_no, count]
 TASK_2_QUERY = """
+select flight_no, count(*) as count
+from flights
+group By flight_no 
+having count(flight_no) < 50
+order by count desc 
+limit 3;
 """
 #  flight_no | count
 # -----------+-------
 #  PG0260    |    27
 #  PG0371    |    27
 #  PG0310    |    27
-
-# Вывести число перелетов внутри одной таймзоны
-# Нужно вывести 1 значение в колонке count
-TASK_3_QUERY = """
-"""
-#  count
-# --------
-#  16824
